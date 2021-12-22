d6bb7a7ed2afd4585b635d9157680f65da077e41
Lee Siang Yin <siangyin916@gmail.com>
2021-12-22 17:56:17 +0800

    🌍 Connect to mongodb v mongoose

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
 const express = require("express")
-
 const app = express()
 const PORT = process.env.PORT || 3030;
+const mongoose = require("mongoose")
+require("dotenv").config()
 
-app.listen(PORT, () => {
- console.log(`connected on ${PORT}`)
-})
+// DATABASE CONNECTION
+
+function connectDB(url) {
+ return mongoose.connect(url).then(() => {
+ console.log("CONNECT TO MONGODB...")
+}).catch(err=>console.log(err))
+}
+
+async function start() {
+ try {
+  await connectDB(process.env.MONGO_URL)
+  app.listen(PORT, () => { console.log(`Server listening on ${PORT}...`) });
+ } catch (err) {
+  console.log(err)
+ }
+}
+
+start();
